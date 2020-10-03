8830633f25e7100e99a96ac4df8122087a0a1e35
Cynthia <cynthia.j.mattingly@gmail.com>
2020-10-03 11:32:34 +0200

    add symfony annotations and first controller

---
+++ imageApi/src/Controller/ImageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+class ImageController
+{
+    /**
+     * @Route("/image/upload")
+     */
+    public function upload()
+    {
+        die('hi');
+    }
+}
